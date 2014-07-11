8dc806b416a1d606730b52cb6754f9a891a20a41
vincent33 <vincent33@acs.com.tw>
2014-07-11 17:04:01 +0800

    plot1

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data <- read.table(".\\household_power_consumption.txt",sep=";",header=TRUE)
+filterData <- subset(data, as.Date(Date,"%d/%m/%Y") == as.Date("01/02/2007","%d/%m/%Y")|as.Date(Date,"%d/%m/%Y") == as.Date("02/02/2007","%d/%m/%Y"))
+chartData <- as.double(levels(filterData$Global_active_power))[filterData$Global_active_power]
+par(mar = c(4,4,4,4))
+hist(chartData,main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
